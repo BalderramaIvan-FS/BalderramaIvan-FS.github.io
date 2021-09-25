f3120a52000a76d50b70290a3377465c2b03a362
Ivan <ibalderrama@student.fullsail.edu>
2021-09-24 21:42:43 -0500

    Added users appointment results

--- main.js
+++ main.js
@@ -2,27 +2,40 @@
 let apptTimes = ["9:00AM - 10:00AM", "10:00AM-11:00AM", "11:00AM- 12:00PM",
 "1:00PM - 2:00PM", "2:00PM - 3:00PM", "3:00PM - 4:00PM", "4:00PM - 5:00PM"];
 
-let stringAppt = "Please select an appointment for today by typing '1,2,3, ect..'\n";
+let stringAppt = "Time Availability:\n\n";
 //Show the user the available items
 for(let i = 0; i < apptTimes.length; i++)
 {
-    stringAppt += "(" + (i+1) + ") " + apptTimes[i] + " \n "
+    stringAppt += "(" + (i+1) + "). " + apptTimes[i] + " \n\n "
 }
 
 //Prompt the user to select a time
-let userInput = Number(prompt(stringAppt))
 
-while(isNaN(userInput) || userInput === "" || userInput < 1 || userInput > apptTimes.length)
-{
-    userInput = Number(prompt("Invalid input. \n" + stringAppt));
-}
-userInput -= 1;
-//Remove that appointment from the list.
-apptTimes.splice(userInput, 1);
+let timeHours = document.getElementById('hours');
+timeHours.innerText = stringAppt;
 
-console.log("The remaining appointments available are: ")
-for(let j = 0; j < apptTimes.length; j++)
-{
+var submitButton = document.getElementById('apptButton').onclick = appointmentForm;
+
+function appointmentForm(){
+    let userName = document.getElementById('nameInput').value;
+    let numberInput = document.getElementById('number').value;
+    let service = document.getElementById('serviceInput').value;
+
+    apptTimes.splice(Number(numberInput -1), 1);
+
     
-    console.log(apptTimes[j]);
+
+
+    document.getElementById("nameResult").innerHTML += userName;
+    document.getElementById("appointmentTime").innerHTML += apptTimes[Number(numberInput -1)];
+    document.getElementById("serviceType").innerHTML += service;
+
 }
+
+
+    for(let j = 0; j < apptTimes.length; j++)
+    {
+        timeHours.innerText += apptTimes[j]+"\n";
+    };
+
+
